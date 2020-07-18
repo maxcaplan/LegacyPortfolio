0a62d1942ec26d989407089d9c4bf3642ab65574
maxcaplan <maxacaplan@gmail.com>
2020-07-18 11:50:01 -0300

    added about me layout

--- js/index.js
+++ js/index.js
@@ -75,3 +75,13 @@ function changePhrase() {
 
   type()
 }
+
+function navbarSize() {
+  if (window.scrollY / window.innerHeight * 100 > 20) {
+    document.getElementById('navbar').classList.add('shrink')
+  } else {
+    document.getElementById('navbar').classList.remove('shrink')
+  }
+}
+
+window.addEventListener("scroll", navbarSize)
--- js/logo.js
+++ js/logo.js
@@ -1,25 +1,26 @@
 new Vivus(
-    "logo-svg",
-    {
-      type: "oneByOne",
-      duration: 100,
-      animTimingFunction: Vivus.EASE
-    },
-    svgAnimEnd
-  );
-  
-  function svgAnimEnd() {
-    document.getElementById('dot-path').classList.add("pulse")
-    document.getElementById('subtitle').classList.add("fade-in-up")
-    document.getElementById('splash').classList.add("fade-out")
+  "logo-svg", {
+    type: "oneByOne",
+    duration: 100,
+    animTimingFunction: Vivus.EASE
+  },
+  svgAnimEnd
+);
 
-    document.getElementById('splash').addEventListener('animationend', event => {
-      if(event.target.id == "splash") {
-        console.log("removing from dom")
-        event.target.remove()
+function svgAnimEnd() {
+  document.getElementById('dot-path').classList.add("pulse")
+  document.getElementById('subtitle').classList.add("fade-in-up")
+  document.getElementById('splash').classList.add("fade-out")
 
-        type()
-      }
-    });
-  }
-  
+  window.scrollTo(0, 0);
+
+  document.getElementById('splash').addEventListener('animationend', event => {
+    if (event.target.id == "splash") {
+      event.target.remove()
+
+      document.body.classList.remove("hide-scroll")
+
+      type()
+    }
+  });
+}
