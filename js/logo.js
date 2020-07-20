02407aeb2d20c0ba5da178c01752a59ceb48756f
maxcaplan <maxacaplan@gmail.com>
2020-07-20 10:15:25 -0300

    header background update

--- js/logo.js
+++ js/logo.js
@@ -11,6 +11,7 @@ function svgAnimEnd() {
   document.getElementById('dot-path').classList.add("pulse")
   document.getElementById('subtitle').classList.add("fade-in-up")
   document.getElementById('splash').classList.add("fade-out")
+  document.getElementById("header-bg").classList.add("fade-in-bottom-right")
 
   window.scrollTo(0, 0);
 
