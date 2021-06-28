3e48c132c0024e2e00e577e9d98ddfe27db569e0
maxacaplan <maxacaplan@gmail.com>
2021-06-28 16:13:21 -0300

    removed unused code from home

--- components/home.vue
+++ components/home.vue
@@ -1,23 +1,23 @@
 <template>
-  <section id="home">
-    <div class="container is-mx-3-touch">
-      <div style="width: 100%;">
-        <div class="columns">
-          <div class="column is-narrow">
-            <div class="px-3">
+  <div id="home-wrapper">
+    <section id="home" class="section">
+      <div class="container">
+        <div style="width: 100%;">
+          <div class="columns">
+            <div class="column is-narrow">
               <h1 class="title is-size-1-touch">
                 Hi. <br />
                 I'm Max,
               </h1>
               <h2 class="subtitle is-size-3-touch">And I like designing</h2>
             </div>
-          </div>
 
-          <div class="column"></div>
+            <div class="column"></div>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -27,22 +27,18 @@ export default {
 </script>
 
 <style scoped>
+#home-wrapper {
+  height: calc(100vh - 74px);
+  width: 100%;
+}
+
 #home {
-  /* height: calc(100% - 74px); */
   height: 100%;
   width: 100%;
-  /* margin-top: 74px; */
   position: absolute;
   top: 0;
   left: 0;
 }
-
-/* @media only screen and (max-width: 1023px) {
-  #home {
-    height: calc(100% - 52px);
-    margin-top: 52px;
-  }
-} */
 
 #home .container {
   height: 100%;
